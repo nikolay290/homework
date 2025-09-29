8999557b015700b3b9df53c0cf467538a418291e
nikolay290 <vnm1705@mail.ru>
2025-09-29 12:19:07 +0300

    Update Task1.1.c

--- Task1.1.c
+++ Task1.1.c
@@ -1,53 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
-/**
- * @brief Ðàññ÷èòûâàåò ôóíêöèþ A ïî çàäàííîé ôîðìóëå
- * @param x - çíà÷åíèå ïàðàìåòðà x
- * @param y - çíà÷åíèå ïàðàìåòðà y
- * @param z - çíà÷åíèå ïàðàìåòðà z
- * @return ðàññ÷èòàííîå çíà÷åíèå
- */
-double getA(const double x, const double y, const double z);
-/**
- * @brief Ðàññ÷èòûâàåò ôóíêöèþ Â ïî çàäàííîé ôîðìóëå
- * @param x - çíà÷åíèå ïàðàìåòðà x
- * @param y - çíà÷åíèå ïàðàìåòðà y
- * @param z - çíà÷åíèå ïàðàìåòðà z
- * @return ðàññ÷èòàííîå çíà÷åíèå
- */
-double getB(const double x, const double y, const double z, double a);
-
-const double x = -0.5;
-const double y = 1.7;
-const double z = 0.44;
-double a;
-double b;
-/**
- * @brief Òî÷êà âõîäà â ïðîãðàììó
- * @return Âîçâðàùàåò 0, åñëè ïðîãðàììà âûïîëíåíà êîððåêòíî
- */
-int main()
+/*
+* @brief Функция проверяет, что второе число отлично от нуля
+*/
+void checkvalue(const double num);
+/*
+* @brief Функция для ввода 2 чисел и вычисления их суммы, разности, произведения, частного.
+* @param num1 - первое число
+* @param num2 - второе число число
+* @return рассчитанное значения
+*/
+int main(void)
 {
-	double a = getA(x, y, z);
-	double b = getB(x, y, z, a);
-	printf("x = %lf\n", x);
-	printf("y = %lf\n", y);
-	printf("z = %lf\n", z);
-	printf("a = %lf\n", a);
-	printf("b = %lf\n", b);
-
+	double num1, num2;
+	printf("Введите первое число ");
+	scanf_s("%lf", &num1);
+	printf("Введите второе число ");
+	scanf_s("%lf", &num2);
+	checkvalue(num2);
+	printf("Сумма: %lf\n", num1 + num2);
+	printf("Разность %lf\n", num1 - num2);
+	printf("Произведение %lf\n", num1 * num2);
+	printf("Частное %lf\n", num1 / num2);
 	return 0;
 }
-
-double getA(const double x, const double y, const double z)
+void checkvalue(const double num)
 {
-	double a = pow(exp(1.0), y * z) * sin(x * z - y) - pow(fabs(y * z + x), 0.5);
-	return a;
-}
-
-double getB(const double x, const double y, const double z, double a)
-{
-	double b = y * sin(a * pow(z, 2) * cos(2 * z)) - 1;
-	return b;
-
+	if (num == 0)
+	{
+		printf("Второе число должно быть отлично от нуля");
+		abort();
+	}
 }
